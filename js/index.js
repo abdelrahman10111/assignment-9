fd142ef4fb792ba983f063214d8524c588f0813c
abdelrahman10111 <abdoelbialy416@gmail.com>
2024-05-27 00:56:43 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var QUOTES = [
+  {
+    teller: "Hamoksha",
+    message: "h hjhjh jhj yjh yyyjy jy",
+  },
+  {
+    teller: "Abdo",
+    message: "h hjhjh jhj yjh yyyjy jy",
+  },
+  {
+    teller: "Hamo",
+    message: "h hjhjh jhj yjh yyyjy jy",
+  },
+  {
+    teller: "Hamada",
+    message: "h hjhjh jhj yjh yyyjy jy",
+  },
+];
+
+function addQuotes() {
+    debugger
+  var index = Math.floor(Math.random() * (QUOTES.length));
+  var quote = QUOTES[index]
+  displayQuote(quote.teller, quote.message);
+}
+
+// function randomInteger(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+function displayQuote(teller, message) {
+  document.getElementById("message").innerHTML = message;
+  document.getElementById("teller").innerHTML = "--" + teller;
+}
